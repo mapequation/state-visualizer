350b575208198a9a237565cc1cbd7ba8db40690e
Anton Eriksson <anton.eriksson@umu.se>
2022-03-31 15:14:00 +0200

    Decay slower during initial iterations

--- src/hooks/useSimulation.ts
+++ src/hooks/useSimulation.ts
@@ -26,16 +26,12 @@ export default function useSimulation({
   linkDistance,
   initialIterations = 100,
 }: UseSimulationOptions): Simulation {
-  const decay = 1 - Math.pow(0.001, 1 / 500);
-
   const simulation = d3
     .forceSimulation(nodes)
     .force("center", d3.forceCenter(2000, 2000))
     .force("collide", d3.forceCollide(2 * nodeRadius))
     .force("charge", d3.forceManyBody().strength(nodeCharge))
     .force("link", d3.forceLink(links).distance(linkDistance));
-
-  simulation.alphaDecay(decay);
 
   const stateSimulation = d3
     .forceSimulation(states)
@@ -49,12 +45,18 @@ export default function useSimulation({
       ).strength(1)
     );
 
-  stateSimulation.alphaDecay(decay);
+  const initialDecay = simulation.alphaDecay();
 
   if (initialIterations) {
+    const decay = 1 - Math.pow(0.001, 1 / 1000);
+    simulation.alphaDecay(decay);
+    stateSimulation.alphaDecay(decay);
     simulation.tick(initialIterations);
     stateSimulation.tick(initialIterations);
   }
+
+  simulation.alphaDecay(initialDecay);
+  stateSimulation.alphaDecay(initialDecay);
 
   return { simulation, stateSimulation };
 }
