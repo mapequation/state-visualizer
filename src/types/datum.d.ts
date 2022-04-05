af4a0c3276d22bce97902c7535d46a353ea5eca9
Anton Eriksson <anton.eriksson@umu.se>
2022-04-05 19:44:17 +0200

    Cleanup types

--- src/types/datum.d.ts
+++ src/types/datum.d.ts
@@ -1,34 +1,32 @@
-import { FlowNode, FlowStateNode, Link } from "../lib/merge-states-clu";
+import type { SimulationLinkDatum, SimulationNodeDatum } from "d3";
+import type { FlowNode, FlowStateNode, Link } from "../lib/merge-states-clu";
 
-export type NodeDatum = FlowNode & {
-  states: StateNodeDatum[];
-  x: number;
-  y: number;
-  fx?: number;
-  fy?: number;
-};
+export type NodeDatum = FlowNode &
+  SimulationNodeDatum & {
+    states: StateNodeDatum[];
+    x: number;
+    y: number;
+  };
 
-export type StateNodeDatum = FlowStateNode & {
-  physicalNode: NodeDatum;
-  x: number;
-  y: number;
-  fx?: number;
-  fy?: number;
-  fill?: string;
-  stroke?: string;
-  radius?: number;
-};
+export type StateNodeDatum = FlowStateNode &
+  SimulationNodeDatum & {
+    physicalNode: NodeDatum;
+    x: number;
+    y: number;
+    fill?: string;
+    stroke?: string;
+    radius?: number;
+  };
 
-export type LinkDatum<NodeType = StateNodeDatum> = Omit<
-  Link,
-  "source" | "target"
-> & {
-  source: NodeType;
-  target: NodeType;
-  width?: number;
-  stroke?: string;
-  isInter?: boolean;
-};
+export type LinkDatum<NodeDatum extends SimulationNodeDatum = StateNodeDatum> =
+  SimulationLinkDatum<NodeDatum> &
+    Omit<Link, "source" | "target"> & {
+      source: NodeDatum;
+      target: NodeDatum;
+      width?: number;
+      stroke?: string;
+      isInter?: boolean;
+    };
 
 export type NetworkDatum = {
   nodes: NodeDatum[];
